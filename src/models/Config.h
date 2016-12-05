624f5f458b34a65b38ff2a4a99b322983ef727d2
valLP <valbho39@hotmail.fr>
2016-12-05 13:13:26 +0100

    Ajout d'exemple et quelques variables

--- src/models/Config.h
+++ src/models/Config.h
@@ -5,8 +5,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <string>
 #include <fstream>
 #include "json.h"
+#include "User.h"
+#include "Backup.h"
 
 class Config {
     nlohmann::json config;
@@ -15,6 +18,19 @@ public:
     {
         std::ifstream conf_file("../VerrouYEAH/src/config.json");
         conf_file >> config;
+    }
+    /*
+     * Exemple de création d'objet à partir du fichier json
+     */
+    User* loadUser(std::string login)
+    {
+        //On se place dans l'objet users,
+        //puis à l'intérieur dans l'objet qui a pour cle le login de l'utilisateur en paramètre
+        nlohmann::json user_config = config.at("users").at(login);
+        //Une fois dans le bon objet json, on recherche la valeur du champ password en type std::string
+        std::string password = user_config.at("password").get<std::string>();
+
+        return new User(login,password);
     }
     std::string toString()
     {
--- src/models/User.h
+++ src/models/User.h
@@ -5,9 +5,18 @@
 #ifndef USER_H
 #define USER_H
 
+#include <string>
+#include <list>
+#include "Backup.h"
 
 class User {
-
+    std::string login;
+    std::string password;
+    std::list<Backup> backups;
+public:
+    User(std::string log,std::string pass):
+            login(log),password(pass)
+    {}
 };
 
 
