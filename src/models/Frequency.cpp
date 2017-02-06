b38a64f787be2ad5b974a6a6805b0f0de0b363f0
valLP <valbho39@hotmail.fr>
2017-02-06 13:16:01 +0100

    Ajout Frequency

--- src/models/Frequency.cpp
+++ src/models/Frequency.cpp
@@ -1,19 +1,148 @@
-//
-// Created by Valentin on 05/12/16.
-//
-
 #include "Frequency.h"
+#include <ctime>
+using namespace std;
 
+void Frequency::setup(){
+    minutes = new char [N_MINUTE];
+    memset(minutes, INIT_VALUE, N_MINUTE);
 
+    hours = new char [N_HOUR];
+    memset(hours, INIT_VALUE, N_HOUR);
 
-void Frequency::operator=(const Frequency &freq){
+    days_of_month = new char[N_DAY_OF_MONTH];
+    memset(days_of_month, INIT_VALUE, N_DAY_OF_MONTH);
 
+    months = new char[N_MONTH];
+    memset(months, INIT_VALUE, N_MONTH);
+
+    days_of_week = new char[N_DAY_OF_WEEK];
+    memset(days_of_week, INIT_VALUE, N_DAY_OF_WEEK);
+}
+char* Frequency::parser(string expr,char size){
+    char* tab = new char[size];
+    memset(tab, 0, size);
+
+    string part;
+    string buffer;
+    stringstream ss_expr(expr);
+
+    while (getline(ss_expr, part, ',')){
+        if(part.find_first_of('-') == string::npos){
+            if(part.compare("*")==0){
+                for(int i=0;i<size;i++)
+                    tab[i]=1;
+                break;
+            }
+            else{
+                int index = stoi(part);
+                tab[index]=1;
+            }
+        }
+        else{
+            char begin  = -1;
+            char end    = -1;
+            stringstream ss_part(part);
+            while (getline(ss_part, buffer, '-')) {
+                if (begin == -1)
+                    begin = stoi(buffer);
+                else
+                    end = stoi(buffer);
+            }
+            for(int k=begin;k<=end;k++)
+                tab[k]=1;
+        }
+    }
+
+    return tab;
 }
 
-bool Frequency::operator==(const Frequency &freq){
+string Frequency::number_to_string(char* tab,char length,char type){
+    string str = "";
+    char empty  =  1;
+    char begin  = -1;
+    char end    = -1;
+    for(char i=0;i<length;i++){
+        if(tab[i]==1){
+            if(begin == -1)
+                begin = i;
+            end = i;
+        } else{
+            if(begin != -1){
+                if(!empty)
+                    str+=",";
 
+                if(end == begin) {
+                    if (type == OFFSET_1_TYPE)
+                        str += to_string(begin+1);
+                    else
+                        str += to_string(begin);
+                }else {
+                    if (type == OFFSET_1_TYPE)
+                        str += to_string(begin+1) + "-" + to_string(end+1);
+                    else
+                        str += to_string(begin) + "-" + to_string(end);
+
+                }
+                empty   =  0;
+                begin   = -1;
+                end     = -1;
+            }
+        }
+    }
+    if(end-begin==length-1)
+        str += "*";
+    else if(empty)
+        str += "0";
+    return str;
 }
+string Frequency::toString(){
+    string freq = number_to_string(minutes,N_MINUTE,NORMAL_TYPE)+" ";
+    freq += number_to_string(hours,N_HOUR,NORMAL_TYPE)+" ";
+    freq += number_to_string(days_of_month,N_DAY_OF_MONTH,OFFSET_1_TYPE)+" ";
+    freq += number_to_string(months,N_MONTH,NORMAL_TYPE)+" ";
+    freq += number_to_string(days_of_week,N_DAY_OF_WEEK,NORMAL_TYPE);
+    return freq;
+}
+Frequency& Frequency::operator=(const Frequency& src){
+    if(this != &src){
+        for(int i=0;i<N_MINUTE;i++)
+            minutes[i] = src.minutes[i];
 
-bool Frequency::operator!=(const Frequency &freq){
+        for(int i=0;i<N_HOUR;i++)
+            hours[i] = src.hours[i];
 
+        for(int i=0;i<N_DAY_OF_MONTH;i++)
+            days_of_month[i] = src.days_of_month[i];
+
+        for(int i=0;i<N_MONTH;i++)
+            months[i] = src.months[i];
+
+        for(int i=0;i<N_DAY_OF_WEEK;i++)
+            days_of_week[i] = src.days_of_week[i];
+    }
+    return *this;
+}
+bool Frequency::contain(char min,char hour,char dom,char month,char dow){
+    //TODO tester les arguments
+    return minutes[min] && hours[hour] && days_of_month[dom-1] && months[month-1] && days_of_week[dow];
+}
+bool Frequency::isNow(){
+    time_t t = time(0);
+    tm * now = localtime(&t);
+    cout << now->tm_min << " " << now->tm_hour << " " << now->tm_mday << " " << now->tm_mon << " " << now->tm_wday << endl;
+    return contain(now->tm_min,now->tm_hour,now->tm_mday,now->tm_mon,now->tm_wday);
+}
+bool Frequency::since(time_t t){
+    time_t now = time(0);
+    tm * date;
+    do{
+        if(t>now)
+            return 0;
+        date = localtime(&t);
+        t+=60;
+
+    }while(!contain(date->tm_min,date->tm_hour,date->tm_mday,date->tm_mon,date->tm_wday));
+    char* dt = asctime(date);
+    cout << "Date de la premiere frequence ratee: "<< dt << endl;
+    return 1;
 }
--- src/models/Frequency.h
+++ src/models/Frequency.h
@@ -1,19 +1,88 @@
-//
-// Created by Valentin on 05/12/16.
-//
-
-#ifndef FREQUENCY_H
-#define FREQUENCY_H
+#ifndef FREQ_FREQUENCY_H
+#define FREQ_FREQUENCY_H
+#include <string>
+#include <cstring>
 
 
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+#define N_MINUTE 60
+#define N_HOUR 24
+#define N_DAY_OF_MONTH 31
+#define N_MONTH 12
+#define N_DAY_OF_WEEK 7
+
+#define NORMAL_TYPE 0
+#define OFFSET_1_TYPE 1
+
+#define INIT_VALUE 1
+/*
+ *   minute
+ *   heure
+ *   jour du mois
+ *   mois
+ *   jour de la semaine
+ *
+ */
 class Frequency {
+    char* minutes;
+    char* hours;
+    char* days_of_month;
+    char* months;
+    char* days_of_week;
 
+    std::string number_to_string(char* tab,char length,char type);
+    void setup();
+    char* parser(std::string expr,char size);
 public:
+    Frequency(){
+        setup();
+    }
+    Frequency(std::string freq){
+        std::string buffer;
+        std::stringstream ss(freq);
+        std::vector<std::string> tokens;
+        while (std::getline(ss, buffer, ' '))
+            tokens.push_back(buffer);
 
-    void operator=(const Frequency&);
-    bool operator==(const Frequency&);
-    bool operator!=(const Frequency&);
+        if(tokens.size() == 5){
+            minutes         = parser(tokens[0],N_MINUTE);
+            hours           = parser(tokens[1],N_HOUR);
+            days_of_month   = parser(tokens[2],N_DAY_OF_MONTH);
+            months          = parser(tokens[3],N_MONTH);
+            days_of_week    = parser(tokens[4],N_DAY_OF_WEEK);
+        }
+    }
+    void setMinutes(std::string str){
+        delete(minutes);
+        minutes = parser(str,N_MINUTE);
+    }
+    void setHours(std::string str){
+        delete(hours);
+        hours = parser(str,N_HOUR);
+    }
+    void setDaysOfMonth(std::string str){
+        delete(days_of_month);
+        days_of_month = parser(str,N_DAY_OF_MONTH);
+    }
+    void setMonths(std::string str){
+        delete(months);
+        months = parser(str,N_MONTH);
+    }
+    void setDaysOfWeek(std::string str){
+        delete(days_of_week);
+        days_of_week = parser(str,N_DAY_OF_WEEK);
+    }
+    Frequency& operator=(const Frequency& target);
+    bool contain(char min,char hour,char dom,char month,char dow);
+    bool isNow();
+    bool since(time_t t);
+    std::string toString();
 };
 
 
-#endif //FREQUENCY_H
+#endif //FREQ_FREQUENCY_H
