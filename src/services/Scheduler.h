c8d2ef07553442c84dd7b86953867bcff2bb46fe
valLP <valbho39@hotmail.fr>
2017-02-19 16:01:06 +0100

    Simplification de l'ajout des Backups d'un User

--- src/services/Scheduler.h
+++ src/services/Scheduler.h
@@ -5,6 +5,7 @@
 #include <thread>
 #include <unistd.h>
 #include "../models/Backup.h"
+#include "../models/User.h"
 
 class Scheduler {
     static Scheduler instance;
@@ -26,6 +27,7 @@ public:
     void start();
     void stop();
     void add(Backup& backup);
+    void addFromUser(User* user);
     void remove(Backup& backup);
     void clear()
     {
